be3e954dd31d696c4c12b30548de99030aefa25b
rsouverain <rsouverain@kaliop.com>
2020-02-01 01:22:09 +0100

    deploying

---
+++ KORIANFREEWIFIAUTOLOGINU.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name KORIAN Free Wifi Autologin Userscript
+// @namespace KORIANFREEWIFIAUTOLOGINU
+// @author rsouverain
+// @description Browser userscript to autologin the free wifi from KORIAN EPHAD
+// @include http://passman02.wifipass.org*
+// @homepage https://github.com/rsouverain/KORIAN_wifipass_autologin_userscript
+// @downloadURL https://raw.githubusercontent.com/rsouverain/KORIAN_wifipass_autologin_userscript/live/KORIANFREEWIFIAUTOLOGINU.user.js
+// @updateURL https://raw.githubusercontent.com/rsouverain/KORIAN_wifipass_autologin_userscript/live/KORIANFREEWIFIAUTOLOGINU.meta.js
+// @icon https://i.imgur.com/v7SKKdU.png
+// @grant none
+// @run-at document-start
+// @version 2020.2.0
+// ==/UserScript==
