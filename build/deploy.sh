3b5370da6aa5831c9b7d7ed036cdffabed4b9d24
rsouverain <rsouverain@kaliop.com>
2020-02-01 01:18:16 +0100

    chore: useless file cleanup and fixing deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -8,6 +8,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploying'
-git push -f https://github.com/rsouverain/deim-vuepress.git master:live
+git push -f https://github.com/rsouverain/KORIAN_wifipass_autologin_userscript.git master:live
 rm -rf .git
 cd -
